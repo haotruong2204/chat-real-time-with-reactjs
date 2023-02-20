8e44c5bf1b4cb9a0c49deea471abc9eff8f5631c
Hao Truong <44367086+haotruong2204@users.noreply.github.com>
2023-02-21 00:44:30 +0700

    Refs #002 Config React: conected websocket (#2)

---
+++ client/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import "./App.css";
+
+const ws = new WebSocket("ws://localhost:3000/cable");
+
+function App() {
+  const [messages, setMessages] = useState([]);
+  const [guid, setGuid] = useState("");
+
+  ws.onopen = () => {
+    console.log("Connected to websocket server");
+    setGuid(Math.random().toString(36).substring(2, 15));
+
+    ws.send(
+      JSON.stringify({
+        command: "subscribe",
+        identifier: JSON.stringify({
+          id: guid,
+          channel: "MessagesChannel",
+        }),
+      })
+    );
+  };
+  return (
+    <div className="App">
+      <div className="messageHeader">
+        <h1>Messages</h1>
+        <p>Guid: {guid}</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
