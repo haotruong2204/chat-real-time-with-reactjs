e617921773a0613a15268716c87df1142940a62a
Hao Truong <haotv03@gmail.com>
2023-02-21 10:24:44 +0700

    Refs #003 Logic render

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 const ws = new WebSocket("ws://localhost:3000/cable");
@@ -6,6 +6,8 @@ const ws = new WebSocket("ws://localhost:3000/cable");
 function App() {
   const [messages, setMessages] = useState([]);
   const [guid, setGuid] = useState("");
+
+  const messagesContainer = document.getElementById("messages");
 
   ws.onopen = () => {
     console.log("Connected to websocket server");
@@ -21,11 +23,79 @@ function App() {
       })
     );
   };
+
+  ws.onmessage = (e) => {
+    const data = JSON.parse(e.data);
+    if (data.type === "ping") return;
+    if (data.type === "welcome") return;
+    if (data.type === "confirm_subscription") return;
+
+    const message = data.message;
+    setMessagesAndScrollDown([...messages, message]);
+  };
+
+  useEffect(() => {
+    fetchMessages();
+  }, []);
+
+  useEffect(() => {
+    resetScroll();
+  }, [messages]);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const body = event.target.message.value;
+
+    event.target.message.value = "";
+
+    await fetch("http://localhost:3000/messages", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ body }),
+    });
+
+    // const data = await response.json();
+    // setMessagesAndScrollDown([...messages, data]);
+  };
+
+  const fetchMessages = async () => {
+    const response = await fetch("http://localhost:3000/messages");
+    const data = await response.json();
+    setMessagesAndScrollDown(data);
+  };
+
+  const setMessagesAndScrollDown = (data) => {
+    setMessages(data);
+    resetScroll();
+  };
+
+  const resetScroll = () => {
+    if (!messagesContainer) return;
+    messagesContainer.scrollTop = messagesContainer.scrollHeight;
+  };
+
   return (
     <div className="App">
       <div className="messageHeader">
         <h1>Messages</h1>
         <p>Guid: {guid}</p>
+      </div>
+      <div className="messages" id="messages">
+        {messages.map((message) => (
+          <div className="message" key={message.id}>
+            <p>{message.body}</p>
+          </div>
+        ))}
+      </div>
+      <div className="messageForm">
+        <form onSubmit={handleSubmit}>
+          <input type="text" name="message" className="messageInput" />
+          <button className="messageButton" type="submit">
+            Send
+          </button>
+        </form>
       </div>
     </div>
   );
